a7fa9723e031772b40972e834d1560db1a0f68cf
Christian B. Knudsen <christian.knudsen@gmail.com>
2024-07-25 13:41:32 +0200

    initial commit

---
+++ udacity/st101/5-piecharts.R
@@ -0,0 +1,20 @@
+# Pie Charts
+# Puha ad bad - lagkagediagrammer...
+
+# Voting 1
+
+# Voting 2
+
+# Voting 3
+
+# Relative data
+
+# Pick the breaks
+
+# Pick the breaks 2
+
+# Build a chart
+
+# Inferring counts
+
+# Summary
